5b8567f9b717f40e0531e91b8d28a826a9fe660f
Wes Powell <wes@treecitytrading.us>
2023-11-30 22:49:21 -0500

    Update xmap-stats.py

--- xmap-stats.py
+++ xmap-stats.py
@@ -3,11 +3,11 @@ name: Run xmap-stats.py
 on:
   push:
     branches:
-      - main  # Adjust to your branch name
+      - main 
 
 jobs:
   build:
-    runs-on: ubuntu-latest  # You can choose a different runner if needed
+    runs-on: ubuntu-latest  
 
     steps:
       - name: Checkout repository
@@ -16,27 +16,27 @@ jobs:
       - name: Set up Python
         uses: actions/setup-python@v2
         with:
-          python-version: 3.x  # Replace with your desired Python version
+          python-version: 3.x 
 
       - name: Install dependencies (if needed)
         run: |
-          pip install -r path/to/requirements/requirements.txt  # Replace with the correct path to requirements.txt
+          pip install -r requirements.txt  # Replace with the correct path to requirements.txt
         working-directory: ${{ github.workspace }}
 
       - name: Run xmap-stats.py
         run: |
           python xmap-stats.py
-        working-directory: ${{ github.workspace }}  # This is the root of your repository
+        working-directory: ${{ github.workspace }}  
 
       - name: Save CSV and JSON artifacts
-        if: success()  # Only run this step if the previous step was successful
+        if: success()  
         run: |
           cp xenmaps_messages.csv ${{ github.workspace }}/xenmaps_messages.csv
           cp xenmaps.json ${{ github.workspace }}/xenmaps.json
         working-directory: ${{ github.workspace }}
 
       - name: Upload CSV and JSON artifacts
-        if: success()  # Only run this step if the previous step was successful
+        if: success()  
         uses: actions/upload-artifact@v2
         with:
           name: xenmaps-artifacts
