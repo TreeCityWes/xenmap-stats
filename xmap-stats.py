01506d91fbbd4063d38d1891e55ce5ebd6c66a03
Wes Powell <wes@treecitytrading.us>
2023-11-30 22:56:22 -0500

    Update xmap-stats.py

--- xmap-stats.py
+++ xmap-stats.py
@@ -1,43 +1,79 @@
-name: Run xmap-stats.py
+import os
+import json
+import csv
+from web3 import Web3
+import sys
 
-on:
-  push:
-    branches:
-      - main 
+# Function to get XenMaps metadata
+def get_xen_maps_metadata(token_id):
+    try:
+        metadata_tuple = xenmaps_contract.functions.getXenMapsMetadata(token_id).call()
+        title, message, mintDate = metadata_tuple
+        return {'token_id': token_id, 'title': title, 'message': message, 'mintDate': mintDate}
+    except Exception as e:
+        print(f"Error fetching metadata for token {token_id}: {e}")
+        return None
 
-jobs:
-  build:
-    runs-on: ubuntu-latest  
+def create_csv_and_json():
+    data = []  # List to store data for CSV and JSON
 
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v2
+    try:
+        with open('xenmaps_messages.csv', 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['TokenID', 'Title', 'Message', 'MintDate'])
 
-      - name: Set up Python
-        uses: actions/setup-python@v2
-        with:
-          python-version: 3.x 
+            for token_id in range(1, total_supply + 1):
+                metadata = get_xen_maps_metadata(token_id)
+                if metadata and "#StayXen" not in metadata['message']:
+                    csv_writer.writerow([metadata['token_id'], metadata['title'], metadata['message'], metadata['mintDate']])
+                    data.append(metadata)
 
-      - name: Install dependencies (if needed)
-        run: |
-          pip install -r ${{ github.workspace }}/requirements.txt  # Replace with the correct path to requirements.txt
-        working-directory: ${{ github.workspace }}
+                    # Green color definition
+                    GREEN = '\033[92m'
+                    RESET = '\033[0m'
 
-      - name: Run xmap-stats.py
-        run: |
-          python xmap-stats.py
-        working-directory: ${{ github.workspace }}  
+                    # Print the token details with green colored values
+                    print(f"Token ID: {GREEN}{metadata['token_id']}{RESET}  Xenmap: {GREEN}{metadata['title']}{RESET} Message: {GREEN}{metadata['message']}{RESET} Date Created: {GREEN}{metadata['mintDate']}{RESET}")
 
-      - name: Save CSV and JSON artifacts
-        if: success()  
-        run: |
-          cp xenmaps_messages.csv ${{ github.workspace }}/xenmaps_messages.csv
-          cp xenmaps.json ${{ github.workspace }}/xenmaps.json
-        working-directory: ${{ github.workspace }}
+                # Write to the CSV file every 20 records
+                if token_id % 20 == 0:
+                    csv_file.flush()  # Flush the buffer to write to the file immediately
 
-      - name: Upload CSV and JSON artifacts
-        if: success()  
-        uses: actions/upload-artifact@v2
-        with:
-          name: xenmaps-artifacts
-          path: xenmaps_messages.csv, xenmaps.json
+            print("CSV file created successfully.")
+
+        # Save data as JSON with the updated file name
+        with open('xenmaps_messages.json', 'w') as json_file:
+            json.dump(data, json_file)
+
+    except Exception as e:
+        print(f"Error creating CSV file or JSON: {e}")
+
+# Connect to X1 FastNet
+rpc_url = 'https://x1-fastnet.infrafc.org'
+w3 = Web3(Web3.HTTPProvider(rpc_url))
+
+if not w3.is_connected():
+    print("Failed to connect to X1 FastNet")
+    exit()
+
+# Load ABIs
+with open('xenmapsABI.json', 'r') as file:
+    xenmaps_abi = json.load(file)
+
+# Contract address
+xenmaps_address = '0x53BE48059b5999d032d0EFbeDe4dcDd637fC5Ec7'
+
+# Create contract instance
+xenmaps_contract = w3.eth.contract(address=xenmaps_address, abi=xenmaps_abi)
+
+# Get total supply
+try:
+    total_supply = xenmaps_contract.functions.totalSupply().call()
+except Exception as e:
+    print(f"Error fetching total supply from the contract: {e}")
+    exit()
+
+# Run CSV and JSON creation
+create_csv_and_json()
+
+sys.exit(0)  # Exit gracefully with a success code
